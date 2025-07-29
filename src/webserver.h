59293f1c052313f4a347bd1c1925942f6e29cf5c
FelixMau <98911066+FelixMau@users.noreply.github.com>
2025-07-29 16:49:19 +0200

    Add goal weight update via web

--- src/webserver.h
+++ src/webserver.h
@@ -8,6 +8,7 @@
 #include "WiFi.h"
 #include "AsyncTCP.h"
 #include "ESPAsyncWebServer.h"
+#include <EEPROM.h>
 #include <secrets.h>
 
 extern struct Shot shot; // Include the Shot structure from shot_stopper.h
@@ -56,9 +57,11 @@ const char index_html[] PROGMEM = R"rawliteral(
   <p>Weight: <span id="weight">%WEIGHT%</span> g</p>
   <p>Pressure: <span id="pressure">%PRESSURE%</span> bar</p>
   <p>Time: <span id="shottime">%SHOTTIME%</span> s</p>
-    <p>Expected End: <span id="expected_end">%EXPECTED_END%</span> s</p>
-    <p>Goal Weight: <span id="goal_weight">%GOAL_WEIGHT%</span> g</p>
-    <p>Weight Offset: <span id="weight_offset">%WEIGHT_OFFSET%</span> g</p>
+  <p>Expected End: <span id="expected_end">%EXPECTED_END%</span> s</p>
+  <p>Goal Weight: <span id="goal_weight">%GOAL_WEIGHT%</span> g</p>
+  <input id="goal_input" type="number" step="0.1" value="%GOAL_WEIGHT%"/>
+  <button onclick="updateGoalWeight()">Set Goal</button>
+  <p>Weight Offset: <span id="weight_offset">%WEIGHT_OFFSET%</span> g</p>
 <script>
   setInterval(()=>fetch('/weight').then(r=>r.text()).then(v=>document.getElementById('weight').textContent=v), 1000);
   setInterval(()=>fetch('/pressure').then(r=>r.text()).then(v=>document.getElementById('pressure').textContent=v), 1000);
@@ -66,6 +69,11 @@ const char index_html[] PROGMEM = R"rawliteral(
     setInterval(()=>fetch('/expected_end').then(r=>r.text()).then(v=>document.getElementById('expected_end').textContent=v), 1000);
     setInterval(()=>fetch('/goal_weight').then(r=>r.text()).then(v=>document.getElementById('goal_weight').textContent=v), 1000);
     setInterval(()=>fetch('/weight_offset').then(r=>r.text()).then(v=>document.getElementById('weight_offset').textContent=v), 1000);
+
+  function updateGoalWeight() {
+    const val = document.getElementById('goal_input').value;
+    fetch('/set_goal_weight?value=' + val, {method: 'GET'});
+  }
 </script>
 </body></html>
 )rawliteral";
@@ -119,6 +127,15 @@ server.on("/goal_weight", HTTP_GET, [](AsyncWebServerRequest *req){
 });
 server.on("/weight_offset", HTTP_GET, [](AsyncWebServerRequest *req){
   req->send(200, "text/plain", readShotWeightOffset());
+});
+server.on("/set_goal_weight", HTTP_GET, [](AsyncWebServerRequest *req){
+  if (req->hasParam("value")) {
+    float gw = req->getParam("value")->value().toFloat();
+    shot.goalWeight = gw;
+    EEPROM.write(WEIGHT_ADDR, (uint8_t)gw);
+    EEPROM.commit();
+  }
+  req->send(200, "text/plain", "OK");
 });
 
   // Server starten
